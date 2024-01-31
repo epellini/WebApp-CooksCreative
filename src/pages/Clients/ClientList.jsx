24c995a5cc13f447bd99959e58b8a7c48e99fd44
ricop7 <enricofpellini@gmail.com>
2024-01-30 19:19:17 -0500

    Connect Supabase

--- src/pages/Clients/ClientList.jsx
+++ src/pages/Clients/ClientList.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 const ClientList = () => {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    getClients();
+  }, []);
+
+
+  async function getClients() {
+    const { data } = await supabase.from("clients").select("*");
+    setClients(data);
+  }
+
   return (
     <div>
-      <h1>Client List Page</h1>
+      <h1>Client List</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <div>Name: {client.name} {client.last_name}</div>
+            <div>Email: {client.email}</div>
+            <div>Phone: {client.phone_number}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
