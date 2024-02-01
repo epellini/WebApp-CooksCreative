39570e71a7c334427ff2a4c441c56377dac8936d
ricop7 <enricofpellini@gmail.com>
2024-02-01 11:00:34 -0500

    Add Supabase Hook

--- src/pages/Clients/ClientList.jsx
+++ src/pages/Clients/ClientList.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import { useSupabase } from "../../hooks/useSupabase";
 
 const ClientList = () => {
   const [clients, setClients] = useState([]);
+  const { supabase } = useSupabase();
 
   useEffect(() => {
     getClients();
   }, []);
-
 
   async function getClients() {
     const { data } = await supabase.from("clients").select("*");
@@ -25,7 +21,9 @@ const ClientList = () => {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <div>Name: {client.name} {client.last_name}</div>
+            <div>
+              Name: {client.name} {client.last_name}
+            </div>
             <div>Email: {client.email}</div>
             <div>Phone: {client.phone_number}</div>
           </li>
