e33a1371c65196d8cc74f2b44de814432e91dc31
Thomas heal <81531490+timmytim101@users.noreply.github.com>
2024-02-03 11:58:35 -0500

    Revert "Add AuthProvider"
    This reverts commit 73044b476b4c6d6e4fa15ee0b6a90bbaa0aca3b4.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
-import { Routes, Route } from "react-router-dom";
-import Sidebar from './components/Sidebar'; // Import the Sidebar component
+import { Routes, Route} from "react-router-dom";
+import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Home from "./pages/Home.jsx";
 import NewClient from "./pages/Clients/NewClient.jsx";
 import Client from "./pages/Clients/Client.jsx";
 import ClientList from "./pages/Clients/ClientList.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import Projects from "./pages/Projects/Projects.jsx";
-import Project from "./pages/Projects/Project.jsx"; // Import the Project component
-import EditProject from "./pages/Projects/EditProject.jsx"; // Import the Edit component
+import Projects from "./pages/Projects.jsx";
+import Login from "./pages/auth/Login.jsx";
+import ProtectedRoute from './ProtectedRoute.jsx';
+
 function App() {
   return (
     <div style={{ display: 'flex' }}>
+      <ProtectedRoute>
       <Header />
-      <Sidebar />
+      <Sidebar/>
+      </ProtectedRoute>
       <main style={{ flexGrow: 1 }}>
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/projects">
-            <Route index element={<Projects/>}/>
-            <Route path=":id" element={<Project/>} />
-            <Route path="edit/:id" element={<EditProject/>} />
-          </Route>
-          <Route path="/clients">
-            <Route index element={<ClientList/>}/>
+          <Route path="/projects" element={ <ProtectedRoute><Projects/></ProtectedRoute>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/clients" >
+            <Route index element={<ProtectedRoute> <ClientList/> </ProtectedRoute>}/>
             <Route path=":id" element={<Client/>}/>
             <Route path="new" element={<NewClient/>}/>
           </Route>
