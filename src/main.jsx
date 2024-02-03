95596a3d3332dce2c8b3e0f516a6b2bfa04ae97d
Thomas heal <81531490+timmytim101@users.noreply.github.com>
2024-02-03 13:05:39 -0500

    Revert "Revert "Merge branch 'develop' of https://github.com/epellini/ccc-app into develop""
    This reverts commit ccfcacef83be3931071509bf080624f3c0e26731.

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
-import * as React from "react";
-import { CssVarsProvider } from "@mui/joy/styles";
-import CssBaseline from "@mui/joy/CssBaseline";
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import { AuthProvider } from "./hooks/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssVarsProvider>
+     <CssVarsProvider>
       <CssBaseline />
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </CssVarsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+     </CssVarsProvider>
   </React.StrictMode>
 );
