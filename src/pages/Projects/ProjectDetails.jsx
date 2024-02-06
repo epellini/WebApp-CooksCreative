476709365d5e6443f495b6197f3b5f11a5d66b87
Thomas heal <81531490+timmytim101@users.noreply.github.com>
2024-02-06 14:36:50 -0500

    Revert "here comes my changes homies"
    This reverts commit a07cd3e87188e2f8e0ef1b0dc69315e887560430.

--- src/pages/Projects/ProjectDetails.jsx
+++ src/pages/Projects/ProjectDetails.jsx
@@ -6,29 +6,13 @@ import Grid from '@mui/joy/Grid';
 import Stack from '@mui/joy/Stack';
 import Button from '@mui/joy/Button';
 import { Link } from "react-router-dom";
-import { DeleteProject } from "../../components/DeleteProject";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-
 const ProjectDetails = () => {
-  const [project, setProject] = useState(null);
-  const handleDelete = async () => {
-    try {
-        if (project && project.project_id) {
-            await DeleteProject(project.project_id);
-            console.log("Project deleted successfully");
-            // Handle any further actions after deletion
-        } else {
-            console.error("Project or project ID is undefined");
-        }
-    } catch (error) {
-        console.error("Error deleting project:", error);
-    }
-};
-
+  const [project, setProject] = useState([]);
 
   useEffect(() => {
     getProject();
@@ -52,9 +36,6 @@ const ProjectDetails = () => {
             <div>Project Name: {project.project_name}</div>
 
             <div>Client Number: {project.client_id}</div>
-
-            <Button onClick={() => handleDelete(project.project_id)}>Delete</Button>
-
           </div>
         ))}
       </ul>
--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export function useSupabase() {
+  return { supabase };
+}
