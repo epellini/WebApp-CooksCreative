2fa4690f612995df48a103719828fba63b53b715
Enrico Pellini <142680134+epellini@users.noreply.github.com>
2024-02-15 12:28:18 -0500

    Broken Login

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useState, useEffect, createContext, ReactNode, useLa
 import { Session, User } from "@supabase/supabase-js";
 import { supabaseClient } from "../../supabase-client";
 
-
 const AuthContext = createContext<{
   session: Session | null;
   user: User | null;
@@ -25,14 +24,15 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const fetchData = async () => {
       const { data: { session }, error } = await supabaseClient.auth.getSession();
       if (error) {
+        console.log('session')
         console.error("Error fetching session:", error.message);
         setLoading(false);
         return;
       }
   
-      setSession(session);
+      //setSession(session);
   
-      if (session?.user) {
+       if (session?.user) {
         const { data: userDetails, error: userDetailsError } = await supabaseClient
           .from('users')
           .select('*')
@@ -47,7 +47,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         } else {
           console.error('Error fetching user details:', userDetailsError?.message);
         }
-      }
+       }
   
       setLoading(false);
     };
@@ -56,7 +56,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   
     const { data: listener } = supabaseClient.auth.onAuthStateChange(
       (_event, session) => {
-        setSession(session);
+       // setSession(session);
         setUser(session?.user || null);
         setLoading(false);
       }
@@ -67,6 +67,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     };
   }, []);
 
+
   const signOut = async () => {
     await supabaseClient.auth.signOut();
   };
@@ -74,6 +75,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   if (loading) {
     // Render a loading indicator or skeleton UI while fetching data
     return <div>Loading...</div>;
+
   }
 
   const value = { session, user, signOut, isAdmin };
