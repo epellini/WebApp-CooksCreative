4aa3fdc871b3ce09afc70544628201569c372052
Enrico Pellini <142680134+epellini@users.noreply.github.com>
2024-03-10 13:26:42 -0400

    Image UI

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { FormGroup } from "@mui/material";
+import { CardContent, CardMedia, FormGroup } from "@mui/material";
 import { FormControl } from "@mui/material";
 import Button from "@mui/joy/Button";
 import { v4 as uuidv4 } from "uuid";
 import { supabaseClient } from "../../supabase-client";
 import { Stack } from "@mui/material";
 import { Box } from "@mui/material";
+import SvgIcon from "@mui/joy/SvgIcon";
+import { styled } from "@mui/joy";
+import Card from "@mui/joy/Card";
+import CircularProgress from "@mui/material/CircularProgress";
+import Snackbar from "@mui/joy/Snackbar";
 
 const Images = (projectid) => {
   const [images, setImages] = useState([]);
@@ -13,7 +18,49 @@ const Images = (projectid) => {
   const [imageUrl, setImageUrl] = useState("");
   const [imageUrls, setImageUrls] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
-  const [refresh, setRefresh] = useState(false); // State variable for triggering refresh
+  const [refresh, setRefresh] = useState(false);
+
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const [isUploading, setIsUploading] = useState(false);
+
+  const VisuallyHiddenInput = styled("input")`
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    height: 1px;
+    overflow: hidden;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    white-space: nowrap;
+    width: 1px;
+  `;
+
+  const viewImage = (imageUrl) => {
+    const link = document.createElement("a");
+    link.href = imageUrl;
+    document.body.appendChild(link);
+    link.click();
+  };
+
+  const downloadImage = async (imageUrl) => {
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+      const downloadUrl = window.URL.createObjectURL(blob);
+      const filename = imageUrl.split("/").pop().split("#")[0].split("?")[0];
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(downloadUrl);
+    } catch (error) {
+      console.error("Error downloading the image:", error);
+    }
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -61,10 +108,23 @@ const Images = (projectid) => {
     fetchData();
   }, [refresh]); // Trigger useEffect when refresh state changes
 
-  const handleFileChange = (e) => {
-    setSelectedFile(e.target.files[0]);
-    const url = URL.createObjectURL(e.target.files[0]);
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    setSelectedFile(file);
+    const url = URL.createObjectURL(file);
     setImageUrl(url);
+    setIsUploading(true); // Indicate upload is starting
+    await handleSubmit(file); // Now passing the file directly
+    setIsUploading(false);
+  };
+
+  const deleteImage = async (imageUrl) => {
+    setSnackbarMessage("THOMAS DO THIS DO FOR MEEE!");
+    setOpenSnackbar(true);
   };
 
   // const deleteImage = async (imageUrl) => {
@@ -93,92 +153,146 @@ const Images = (projectid) => {
   //   console.log("Image deleted successfully");
   // };
 
-  const handleSubmit = async () => {
-    if (selectedFile) {
-      const { data, error } = await supabaseClient.storage
-        .from("images")
-        .upload(
-          `project-images/${project.project_id}/${uuidv4()}`,
-          selectedFile
-        );
+  const handleSubmit = async (file) => {
+    const { data, error } = await supabaseClient.storage
+      .from("images")
+      .upload(`project-images/${project.project_id}/${uuidv4()}`, file);
 
-      if (data) {
-        const path = data.path;
-        await supabaseClient.from("images").insert({
-          image_urls: path,
-          project_id: project.project_id,
-        });
-        setImageUrls([...imageUrls, path]);
-        setSelectedFile(null);
-        setImageUrl("");
-        setRefresh(!refresh); // Toggle refresh state to trigger useEffect
-      } else {
-        console.error("Error uploading image", error);
-      }
+    if (data) {
+      const path = data.path;
+      await supabaseClient.from("images").insert({
+        image_urls: path,
+        project_id: project.project_id,
+      });
+      setImageUrls([...imageUrls, path]);
+      setSelectedFile(null); 
+      setImageUrl(""); 
+      setRefresh(!refresh);
+      setSnackbarMessage("Image Uploaded Successfully!");
+      setOpenSnackbar(true);
+    } else {
+      console.error("Error uploading image", error);
     }
   };
 
   return (
     <div>
-      {imageUrl && (
-        <img
-          src={imageUrl}
-          alt="Preview"
-          style={{
-            height: 233,
-            width: 350,
-            maxHeight: { xs: 233, md: 167 },
-            maxWidth: { xs: 350, md: 250 },
-          }}
-        />
+      <Snackbar
+        open={openSnackbar}
+        onClose={() => setOpenSnackbar(false)}
+        autoHideDuration={5000} 
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        {snackbarMessage}
+      </Snackbar>{" "}
+      {isUploading ? (
+        <CircularProgress />
+      ) : (
+        imageUrl && (
+          <img
+            src={imageUrl}
+            alt="Preview"
+            style={{
+              height: 233,
+              width: 350,
+              maxHeight: { xs: 233, md: 167 },
+              maxWidth: { xs: 350, md: 250 },
+            }}
+          />
+        )
       )}
       <FormGroup>
         <FormControl>
-          <input
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </FormControl>
+
+        <Button
+          component="label"
+          role={undefined}
+          tabIndex={-1}
+          variant="outlined"
+          color="neutral"
+          startDecorator={
+            <SvgIcon>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                />
+              </svg>
+            </SvgIcon>
+          }
+        >
+          Upload an image
+          <VisuallyHiddenInput
             type="file"
             accept="image/png, image/jpeg"
             onChange={handleFileChange}
             style={{ display: "none" }}
             id="upload-image-input"
           />
-          <label htmlFor="upload-image-input">
-            <Button component="span">Upload Image</Button>
-          </label>
-        </FormControl>
-
-          <FormControl>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
-          </FormControl>
-
+        </Button>
       </FormGroup>
       <h2>Images</h2>
       <Stack
         direction="row"
+        flexWrap="wrap"
         justifyContent="center"
         alignItems="center"
         spacing={2}
       >
         {imageUrls.map((imageUrl) => (
-          <Box key={imageUrl}>
-            <img
+          <Card
+            variant="outlined"
+            key={imageUrl}
+            sx={{
+              width: 225,
+              height: "auto",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <CardMedia
+              component="img"
               src={imageUrl}
               alt="Uploaded"
-              style={{ height: 233, width: 350 }}
+              sx={{
+                borderRadius: "8px",
+                height: 200,
+                objectFit: "cover",
+                cursor: "pointer",
+              }}
+              onClick={() => viewImage(imageUrl)}
             />
-            {/* <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => deleteImage(imageUrl)}
+            <CardContent
+              sx={{ padding: "2px", "&:last-child": { paddingBottom: "2px" } }}
             >
-              Delete
-            </Button> */}
-          </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                  padding: "2px",
+                }}
+              >
+                <Button color="danger" onClick={() => deleteImage(imageUrl)}>
+                  Delete
+                </Button>
+                <Button color="primary" onClick={() => downloadImage(imageUrl)}>
+                  Download
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
         ))}
       </Stack>
     </div>
