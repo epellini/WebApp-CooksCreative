8ac44f83b0dca116541bfd3c2bff2814366cbf16
Enrico Pellini <142680134+epellini@users.noreply.github.com>
2024-03-19 08:33:11 -0400

    Add login page re-direct

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -59,196 +59,194 @@ function Login() {
     }
   };
 
+  useEffect(() => {
+    if (session) {
+      navigate('/');
+    }
+  }, [session, navigate]);
+
   if (loading) return <CircularProgress />;
   return (
     <CssVarsProvider>
-      {session ? (
-        <>
-          <div>
-            <h2>Welcome back</h2>
-          </div>
-        </>
-      ) : (
-        <>
-          <CssBaseline />
-          <GlobalStyles
-            styles={{
-              ":root": {
-                "--Collapsed-breakpoint": "769px",
-                "--Cover-width": "50vw",
-                "--Form-maxWidth": "800px",
-                "--Transition-duration": "0.4s",
-              },
-            }}
-          />
+      <>
+        <CssBaseline />
+        <GlobalStyles
+          styles={{
+            ":root": {
+              "--Collapsed-breakpoint": "769px",
+              "--Cover-width": "50vw",
+              "--Form-maxWidth": "800px",
+              "--Transition-duration": "0.4s",
+            },
+          }}
+        />
 
+        <Box
+          sx={(theme) => ({
+            width:
+              "clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)",
+            transition: "width var(--Transition-duration)",
+            transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+            position: "relative",
+            zIndex: 1,
+            display: "flex",
+            justifyContent: "flex-end",
+            backdropFilter: "blur(12px)",
+            backgroundColor: "rgba(255 255 255 / 0.2)",
+            [theme.getColorSchemeSelector("dark")]: {
+              backgroundColor: "rgba(19 19 24 / 0.4)",
+            },
+          })}
+        >
           <Box
-            sx={(theme) => ({
-              width:
-                "clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)",
-              transition: "width var(--Transition-duration)",
-              transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
-              position: "relative",
-              zIndex: 1,
+            sx={{
               display: "flex",
-              justifyContent: "flex-end",
-              backdropFilter: "blur(12px)",
-              backgroundColor: "rgba(255 255 255 / 0.2)",
-              [theme.getColorSchemeSelector("dark")]: {
-                backgroundColor: "rgba(19 19 24 / 0.4)",
-              },
-            })}
+              flexDirection: "column",
+              minHeight: "100dvh",
+              width:
+                "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
+              maxWidth: "100%",
+              px: 2,
+            }}
           >
             <Box
+              component="header"
               sx={{
+                py: 3,
                 display: "flex",
-                flexDirection: "column",
-                minHeight: "100dvh",
-                width:
-                  "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
-                maxWidth: "100%",
-                px: 2,
+                alignItems: "left",
+                justifyContent: "space-between",
               }}
             >
-              <Box
-                component="header"
-                sx={{
-                  py: 3,
-                  display: "flex",
-                  alignItems: "left",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Box sx={{ gap: 2, display: "flex", alignItems: "center" }}>
-                  <IconButton variant="soft" color="primary" size="sm">
-                    <BadgeRoundedIcon />
-                  </IconButton>
-                  <Typography level="title-lg">
-                    Cooks Creative Contracting
-                  </Typography>
-                </Box>
+              <Box sx={{ gap: 2, display: "flex", alignItems: "center" }}>
+                <IconButton variant="soft" color="primary" size="sm">
+                  <BadgeRoundedIcon />
+                </IconButton>
+                <Typography level="title-lg">
+                  Cooks Creative Contracting
+                </Typography>
               </Box>
-              <Box
-                component="main"
-                sx={{
-                  my: "auto",
-                  py: 2,
-                  pb: 5,
+            </Box>
+            <Box
+              component="main"
+              sx={{
+                my: "auto",
+                py: 2,
+                pb: 5,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                width: 400,
+                maxWidth: "100%",
+                mx: "auto",
+                borderRadius: "sm",
+                "& form": {
                   display: "flex",
                   flexDirection: "column",
                   gap: 2,
-                  width: 400,
-                  maxWidth: "100%",
-                  mx: "auto",
-                  borderRadius: "sm",
-                  "& form": {
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 2,
-                  },
-                  [`& .${formLabelClasses.asterisk}`]: {
-                    visibility: "hidden",
-                  },
-                }}
-              >
-                <Stack gap={4} sx={{ mb: 2 }}>
-                  <Stack gap={1}>
-                    <Typography level="h3">Sign in</Typography>
-                  </Stack>
+                },
+                [`& .${formLabelClasses.asterisk}`]: {
+                  visibility: "hidden",
+                },
+              }}
+            >
+              <Stack gap={4} sx={{ mb: 2 }}>
+                <Stack gap={1}>
+                  <Typography level="h3">Sign in</Typography>
                 </Stack>
-                <Divider
-                  sx={(theme) => ({
-                    [theme.getColorSchemeSelector("light")]: {
-                      color: { xs: "#FFF", md: "text.tertiary" },
-                      "--Divider-lineColor": {
-                        xs: "#FFF",
-                        md: "var(--joy-palette-divider)",
-                      },
+              </Stack>
+              <Divider
+                sx={(theme) => ({
+                  [theme.getColorSchemeSelector("light")]: {
+                    color: { xs: "#FFF", md: "text.tertiary" },
+                    "--Divider-lineColor": {
+                      xs: "#FFF",
+                      md: "var(--joy-palette-divider)",
                     },
-                  })}
-                ></Divider>
-                <Stack gap={4} sx={{ mt: 2 }}>
-                  <form
-                    onSubmit={(event: React.FormEvent<SignInFormElement>) => {
-                      event.preventDefault();
-                      const formElements = event.currentTarget.elements;
-                      handleLogin(
-                        formElements.email.value,
-                        formElements.password.value
-                      );
-                      const data = {
-                        email: formElements.email.value,
-                        password: formElements.password.value,
-                        persistent: formElements.persistent.checked,
-                      };
-                    }}
-                  >
-                    <FormControl required>
-                      <FormLabel>Email</FormLabel>
-                      <Input type="email" name="email" />
-                    </FormControl>
-                    <FormControl required>
-                      <FormLabel>Password</FormLabel>
-                      <Input type="password" name="password" />
-                    </FormControl>
-                    <Stack gap={4} sx={{ mt: 2 }}>
+                  },
+                })}
+              ></Divider>
+              <Stack gap={4} sx={{ mt: 2 }}>
+                <form
+                  onSubmit={(event: React.FormEvent<SignInFormElement>) => {
+                    event.preventDefault();
+                    const formElements = event.currentTarget.elements;
+                    handleLogin(
+                      formElements.email.value,
+                      formElements.password.value
+                    );
+                    const data = {
+                      email: formElements.email.value,
+                      password: formElements.password.value,
+                      persistent: formElements.persistent.checked,
+                    };
+                  }}
+                >
+                  <FormControl required>
+                    <FormLabel>Email</FormLabel>
+                    <Input type="email" name="email" />
+                  </FormControl>
+                  <FormControl required>
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" name="password" />
+                  </FormControl>
+                  <Stack gap={4} sx={{ mt: 2 }}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
                       <Box
                         sx={{
                           display: "flex",
+                          textAlign: "center",
                           justifyContent: "space-between",
                           alignItems: "center",
                         }}
                       >
-                        <Box
-                          sx={{
-                            display: "flex",
-                            textAlign: "center",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                          }}
-                        >
-                          <Link to="/forgot-password">
-                            Forgot your password?
-                          </Link>
-                        </Box>
+                        <Link to="/forgot-password">
+                          Forgot your password?
+                        </Link>
                       </Box>
-                      <Button fullWidth type="submit">
-                        Sign in
-                      </Button>
-                    </Stack>
-                  </form>
-                </Stack>
-              </Box>
-              <Box component="footer" sx={{ py: 3 }}>
-                <Typography level="body-xs" textAlign="center">
-                  © Cooks Creative Contracting {new Date().getFullYear()}
-                </Typography>
-              </Box>
+                    </Box>
+                    <Button fullWidth type="submit">
+                      Sign in
+                    </Button>
+                  </Stack>
+                </form>
+              </Stack>
+            </Box>
+            <Box component="footer" sx={{ py: 3 }}>
+              <Typography level="body-xs" textAlign="center">
+                © Cooks Creative Contracting {new Date().getFullYear()}
+              </Typography>
             </Box>
           </Box>
-          <Box
-            sx={(theme) => ({
-              height: "100%",
-              position: "fixed",
-              right: 0,
-              top: 0,
-              bottom: 0,
-              left: "clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))",
-              transition:
-                "background-image var(--Transition-duration), left var(--Transition-duration) !important",
-              transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
-              backgroundColor: "background.level1",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
+        </Box>
+        <Box
+          sx={(theme) => ({
+            height: "100%",
+            position: "fixed",
+            right: 0,
+            top: 0,
+            bottom: 0,
+            left: "clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))",
+            transition:
+              "background-image var(--Transition-duration), left var(--Transition-duration) !important",
+            transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+            backgroundColor: "background.level1",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundImage: "url(/images/ccc-login.jpg)",
+            [theme.getColorSchemeSelector("dark")]: {
               backgroundImage: "url(/images/ccc-login.jpg)",
-              [theme.getColorSchemeSelector("dark")]: {
-                backgroundImage: "url(/images/ccc-login.jpg)",
-              },
-            })}
-          />
-        </>
-      )}
+            },
+          })}
+        />
+      </>
     </CssVarsProvider>
   );
 }
