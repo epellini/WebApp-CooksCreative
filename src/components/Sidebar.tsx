2d046921d5fb56f695e08783ccf9cd309842bdca
Enrico Pellini <142680134+epellini@users.noreply.github.com>
2024-03-24 17:38:45 -0400

    Change NavBar Icon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,41 +1,27 @@
 import * as React from "react";
 import GlobalStyles from "@mui/joy/GlobalStyles";
-import Avatar from "@mui/joy/Avatar";
 import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Card from "@mui/joy/Card";
-import Chip from "@mui/joy/Chip";
 import Divider from "@mui/joy/Divider";
 import IconButton from "@mui/joy/IconButton";
-import Input from "@mui/joy/Input";
-import LinearProgress from "@mui/joy/LinearProgress";
 import List from "@mui/joy/List";
 import ListItem from "@mui/joy/ListItem";
 import ListItemButton, { listItemButtonClasses } from "@mui/joy/ListItemButton";
 import ListItemContent from "@mui/joy/ListItemContent";
 import Typography from "@mui/joy/Typography";
 import Sheet from "@mui/joy/Sheet";
-import Stack from "@mui/joy/Stack";
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded";
-import QuestionAnswerRoundedIcon from "@mui/icons-material/QuestionAnswerRounded";
-import GroupRoundedIcon from "@mui/icons-material/GroupRounded";
-import SupportRoundedIcon from "@mui/icons-material/SupportRounded";
-import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import DashboardIcon from "@mui/icons-material/Dashboard";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import BrightnessAutoRoundedIcon from "@mui/icons-material/BrightnessAutoRounded";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { Link } from "react-router-dom";
 import ColorSchemeToggle from "./ColorSchemeToggle";
 import { closeSidebar } from "../utils";
 import { useNavigate } from "react-router-dom";
 import { supabaseClient } from "../supabase-client";
 import { useAuth } from "../pages/Auth/Auth";
+import ConstructionIcon from '@mui/icons-material/Construction';
 
 function Toggler({
   defaultExpanded = false,
@@ -78,8 +64,6 @@ interface User {
 export default function Sidebar() {
   const navigate = useNavigate();
   const { isAdmin, user } = useAuth();
-
-
 
   const Logout = async () => {
     const { error } = await supabaseClient.auth.signOut();
@@ -145,8 +129,8 @@ export default function Sidebar() {
         onClick={() => closeSidebar()}
       />
       <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-        <IconButton variant="soft" color="primary" size="sm">
-          <BrightnessAutoRoundedIcon />
+        <IconButton variant="soft" color="primary" size="sm" component={Link} to="/">
+          <ConstructionIcon />
         </IconButton>
         <Typography level="title-lg">Cooks CC</Typography>
         <ColorSchemeToggle sx={{ ml: "auto" }} />
@@ -211,14 +195,9 @@ export default function Sidebar() {
         </List>
       </Box>
       <Divider />
-      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-        <Avatar
-          variant="outlined"
-          size="sm"
-          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-        />
+      <Box sx={{ display: "flex", gap: 1, alignItems: "left" }}>
         <Box sx={{ minWidth: 0, flex: 1 }}>
-          <Typography level="title-sm">
+          <Typography  level="title-sm">
             {user?.first_name || "Default Name"}
           </Typography>
           <Typography level="body-xs">
